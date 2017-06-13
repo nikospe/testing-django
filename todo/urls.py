4e86f80251f88c4bf9ee2b845ac69376aee561ad
Nikos Perivolaris <nikosper.gm@gmail.com>
2017-06-13 22:30:27 +0300

    init factionality

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_all, name='index')
+    url(r'^$', views.get_all, name='index'),
+    url(r'(?P<id>[0-9]+)', views.get_single, name='single'),
+    url(r'^post/(?P<text>\w{1,150})', views.post, name="post"),
+    url(r'^delete/$', views.delete, name='delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,11 @@
 import json
+import sys
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Todo
 
 def get_all(request):
     todos = Todo.objects.all()
-
     json_array = []
 
     for todo in todos:
@@ -15,7 +14,32 @@ def get_all(request):
     return HttpResponse(json.dumps(json_array), content_type='application/json')
 
 # def get_single
+def get_single(request, id):
+    todo = Todo.objects.get(pk=id)
+
+    return HttpResponse(json.dumps(todo.to_json()))
 
 # def post
+def post(request, text):
+    todo = Todo(text=text)
+    todo.save()
+
+    todos = Todo.objects.all()
+    json_array = []
+
+    for todo in todos:
+        json_array.append(todo.to_json())
+
+    return HttpResponse(json.dumps(json_array), content_type='application/json')    
 
 # def delete
+def delete(request):
+    Todo.objects.all().delete()
+    todos = Todo.objects.all()
+
+    json_array = []
+
+    for todo in todos:
+        json_array.append(todo.to_json())
+
+    return HttpResponse(json.dumps(json_array), content_type='application/json')
